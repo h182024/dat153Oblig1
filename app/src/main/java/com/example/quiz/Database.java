0b87fa403b4dba36cdbd1e61e648e007d54831b4
h182024 <hansopdahl@gmail.com>
2019-01-28 13:49:17 +0100

    Version 1.50

--- app/src/main/java/com/example/quiz/Database.java
+++ app/src/main/java/com/example/quiz/Database.java
@@ -2,6 +2,7 @@ package com.example.quiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.quiz.Person
 
 public class Database extends AppCompatActivity {
 
---
+++ app/src/test/java/com/example/quiz/DatabaseTest.java
@@ -0,0 +1,7 @@
+package com.example.quiz;
+
+import static org.junit.Assert.*;
+
+public class DatabaseTest {
+
+}
